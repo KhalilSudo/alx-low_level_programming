81ca2710aa23b7dbf2f8c9aedf9f6bef014c30e6
KhalilSudo <KhalilSudo@github.com>
2023-02-16 22:29:58 +0100

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,13 +4,14 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- **/
+ */
 int main(void)
 {
-printf("Size of a char:%lu byte(s)\n", sizeof(char));
-printf("Size of a int:%lu byte(s)\n", sizeof(int));
-printf("Size of a long int:%lu byte(s)\n", sizeof(long int));
-printf("Size of a long long:%lu byte(s)\n", sizeof(long long));
-printf("Size of a float:%lu byte(s)\n", sizeof(float));
-return (0);
+  printf("Size of a char:%u byte(s)\n", sizeof(char));
+  printf("Size of an int:%u byte(s)\n", sizeof(int));
+  printf("Size of a long int:%u byte(s)\n", sizeof(long int));
+  printf("Size of a long long int:%u byte(s)\n", sizeof(long long int));
+  printf("Size of a float:%u byte(s)\n", sizeof(float));
+  return (0);
 }
+
