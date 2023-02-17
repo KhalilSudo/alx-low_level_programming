8a41ab94c26ab1e2e9de094e320ffc4700ebc47c
KhalilSudo <KhalilSudo@github.com>
2023-02-18 00:47:27 +0100

    done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This function generates a random integer and prints the value ofitsastdigit,
+ * along with a message indicating whether the last digit is greater than 5, 0,
+ * or less than 6 and not 0.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+for (n = 122; n > 96; n--)
+putchar(n);
+putchar('\n');
+return (0);
+}
