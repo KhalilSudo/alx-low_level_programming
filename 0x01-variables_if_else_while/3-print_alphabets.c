f608d5d27c919286b936633993412a558c2c6fbb
KhalilSudo <KhalilSudo@github.com>
2023-02-17 23:58:45 +0100

    done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,16 +12,16 @@
  */
 int main(void)
 {
-  int n;
+int n;
 
-  for (n = 97; n < 123; n++)
-    putchar(n);
+for (n = 97; n < 123; n++)
+putchar(n);
 
-  for (n = 65; n < 91 ; n++)
-    putchar(n);
+for (n = 65; n < 91 ; n++)
+putchar(n);
 
-  putchar('\n');
+putchar('\n');
 
-  return (0);
+return (0);
 }
 
