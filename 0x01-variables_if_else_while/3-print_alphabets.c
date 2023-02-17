dbfed0dafdb921c1abeaf0ea4deeee2e78f90808
KhalilSudo <KhalilSudo@github.com>
2023-02-17 23:56:43 +0100

    done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * This function generates a random integer and prints the value ofitsastdigit,
- * along with a message indicating whether the last digit is greater than 5, 0,
- * or less than 6 and not 0.
+ * This function generates a random integer and prints the value of its last
+ * digit, along with a message indicating whether the last digit is greater
+ * than 5, 0, or less than 6 and not 0.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n;
+  int n;
 
-for (n = 97; n < 123; n++)
-putchar(n);
-for (n = 65; n < 91 ; n++)
-putchar(n);
-putchar('\n');  
-return (0);
+  for (n = 97; n < 123; n++)
+    putchar(n);
+
+  for (n = 65; n < 91 ; n++)
+    putchar(n);
+
+  putchar('\n');
+
+  return (0);
 }
+
