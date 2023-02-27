a6abb89173da4f57cfb5eea8ca14d01d26b9ff1c
KhalilSudo <KhalilSudo@github.com>
2023-02-27 21:27:08 +0100

    done

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,12 @@
 
 
 /**
- * main - check the code 
+ * reset_to_98 - reset variable to 98
  *
- * Return: Always 0.
+ * @n: integer pointer
  */
 void reset_to_98(int *n)
 {
 
-    *n = 98;
+	*n = 98;
 }
