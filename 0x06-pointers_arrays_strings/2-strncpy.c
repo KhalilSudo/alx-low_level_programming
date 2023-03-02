aafddc53c8cb5f36b266cdb8f7597f20a65dff24
KhalilSudo <KhalilSudo@github.com>
2023-03-02 22:58:21 +0100

    done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,20 @@
 #include "main.h"
-
 /**
- * _strncpy - Copy a string up to n bytes.
- * @dest: Pointer to destination string.
- * @src: Pointer to source string.
- * @n: Number of bytes to copy.
- * Return: Pointer to destination string.
+ * _strncpy - copies a string
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes from src.
+ * Return: the pointer to dest.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-    int src_length, i;
-	
-    for (i = 0;i < n && src[src_length] != '\0'; i++)
-		dest[i] = src[i];
-	
-    for ( ; i < n; i++)
-		dest[i] = '\0';
-	
-    return dest;
-}
+	int i;
 
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
