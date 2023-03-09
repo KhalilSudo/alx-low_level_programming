60737a434f84080c229c0225001c304af5068378
KhalilSudo <KhalilSudo@github.com>
2023-03-09 21:59:32 +0100

    done

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints a length of string
+ *
+ * @s: string
+ *
+ * Return: int length of string
+*/
+
+int _strlen_recursion(char *s);
+{
+int i = 0;
+
+if (*s == '\0')
+{
+return;
+}
+else
+{
+_strlen_recursion(char *s);
+return (i++);
+}
+}
+
