f9befd6b09b0d6cb9a1bc647e101f7c3422fdd49
KhalilSudo <KhalilSudo@github.com>
2023-03-10 23:39:03 +0100

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *@argv: cmd line argument
+ *@argc:number of argument
+ * Return: Always 0 (Success)
+ **/
+int main(int argc, char *argv[})
+{
+printf("%d\n", argc);
+return (0);
+}
