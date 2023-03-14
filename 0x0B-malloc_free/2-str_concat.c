df9dbac023a7e38d130c3372660095e27e106ae4
KhalilSudo <KhalilSudo@github.com>
2023-03-14 23:37:24 +0100

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,43 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * get_length - Returns the length of a string
+ * getLength - Returns the length of a string
  * @str: The string to get the length of
  *
  * Return: The length of the string
  */
-int get_length(char *str)
+int getLength(char *str)
 {
     int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-        ;
+    for (i = 0; str[i] != '\0'; i++);
     return i;
 }
 
 /**
  * str_concat - Concatenates two strings
- * @s1: The first string
- * @s2: The second string
+ * @s1: The first string to concatenate
+ * @s2: The second string to concatenate
+ *
+ * If s1 or s2 is NULL, treat it as an empty string.
  *
  * Return: A pointer to the concatenated string
  */
 char *str_concat(char *s1, char *s2)
 {
+    if (s1 == NULL)
+    {
+        s1 = malloc(sizeof(char));
+        s1[0] = '\0';
+    }
+    if (s2 == NULL)
+    {
+        s2 = malloc(sizeof(char));
+        s2[0] = '\0';
+    }
+
     char *concatenated;
     int i, j, size;
 
-    size = get_length(s1) + get_length(s2);
+    size = getLength(s1) + getLength(s2);
     concatenated = malloc(sizeof(char) * (size + 1));
+
     for (i = 0; s1[i] != '\0'; i++)
     {
         concatenated[i] = s1[i];
     }
+
     for (j = 0; s2[j] != '\0'; j++)
     {
         concatenated[i + j] = s2[j];
     }
+
     concatenated[i + j] = '\0';
     return concatenated;
 }
