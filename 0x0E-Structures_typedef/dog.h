972cded514abebcabbe72ce5c2ea568244932656
khalilSudo <khalilregui19@gmail.com>
2023-03-20 22:46:12 +0000

    done

---
+++ 0x0E-Structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
