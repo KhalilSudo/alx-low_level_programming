729d8150ce58eeda67c037aa8781c1357897e8f0
khalilSudo <khalilregui19@gmail.com>
2023-03-20 23:30:35 +0000

    done

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+/**
+* init_dog - function that int value information of a dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+* @d: pointer to struct dog
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
