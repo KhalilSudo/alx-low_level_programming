ba080d9c27121889b3f0769adff9550e08edd4ca
khalilSudo <khalilregui19@gmail.com>
2023-03-31 22:30:49 +0000

    done

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,5 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t ;
 size_t print_list(const list_t *h);
+
 #endif
