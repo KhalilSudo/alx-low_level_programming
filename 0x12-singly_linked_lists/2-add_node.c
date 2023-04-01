bd6ca640a3a2dc1d6106e3ab18cafcf6cbcaf481
khalilSudo <khalilregui19@gmail.com>
2023-04-01 00:25:22 +0000

    done

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,7 +16,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	newNode->str = strdup(str);
-	newNode->str = strlen(str);
+	newNode->len = strlen(str);
 	if (newNode->str == NULL)
 	{
 		free(newNode);
