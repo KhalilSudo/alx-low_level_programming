a61cfad5110d497e71ae1ffd65e71a4ca8d89c39
khalilSudo <khalilregui19@gmail.com>
2023-06-09 23:47:13 +0100

    DONE

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -11,5 +12,5 @@ int main(int argc, char *argv[])
 {
     printf("%s\n", argv[0]);
 
-    return (0);
+    return EXIT_SUCCESS;
 }
