52a6a1e83c037ea8812cf7250fd33d83fc5cd227
khalilSudo <khalilregui19@gmail.com>
2023-06-10 00:40:56 +0100

    done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of arguments passed to the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+    if(argc < 3)
+    {
+        printf("Error");
+        return (1);
+    }
+    else
+    {
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+    }
+return (0);
+}
+
