2e5eb3b6bd9086b0a7b6463bf58212b4e93e4825
khalilSudo <khalilregui19@gmail.com>
2023-06-10 00:48:41 +0100

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,15 @@
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-    if(argc != 3)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    else
-    {
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
 printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-    }
+}
 return (0);
 }
 
