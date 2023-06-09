6fa9d09b20793331ad6c3b18a09281bb5bdee667
khalilSudo <khalilregui19@gmail.com>
2023-06-10 00:10:54 +0100

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of arguments passed to the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
