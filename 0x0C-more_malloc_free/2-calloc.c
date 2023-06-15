5a5fe48151cf448b981f6a953fb5f89e431b7d74
khalilSudo <khalilregui19@gmail.com>
2023-06-15 23:51:11 +0100

    done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
@@ -24,7 +24,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	for (i = 0; i < nmemb * size; i++)
-		ptr[i] = 0;
+		*((char *)ptr + i) = 0;
 
 	return (ptr);
 }
