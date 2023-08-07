6b91580acc444e5cbf673ee62988d9bad51bece4
KhalilSudo <khalilregui19%40gmail.com>
2023-08-07 21:57:12 +0100

    done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,6 @@
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int i = 0;
-    int length = 0;
-    unsigned int power;
-    unsigned int result = 0;
-
-    while (b[length] != '\0')
-    {
-        length++;
-    }
-    power = length - 1;
-
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] == '1')
-        {
-            result += 1 << power;
-        }
-        power--;
+   
     }
     return result;
 }
