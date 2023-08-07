680bf2af39a4a9a2b82e084995ee89c10a83417d
KhalilSudo <khalilregui19%40gmail.com>
2023-08-07 22:55:26 +0100

    done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int status;
 
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 
 	if (filename == NULL)
 		return (-1);
