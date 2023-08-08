4bd674b5ccf7dd62c06e1ee1f68ecf231993df04
KhalilSudo <khalilregui19%40gmail.com>
2023-08-08 22:36:26 +0100

    DONE

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	for (length = 0; text_content[length] != '\0'; length++)
 		;
 
-	file = open(filename, O_WRONLY | O_APPEND);
+	file = open(filename, O_WRONLY | O_APPEND |O_CREAT);
 	if (file == -1)
 		return (-1);
 
